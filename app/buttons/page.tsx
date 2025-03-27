b910292881e8da085f7d2080165586c6fbd9e774
usernameKo2 <jolom724@gmail.com>
2025-03-27 18:06:41 +0800

    ulit

---
+++ app/buttons/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+    return(
+        <div className="p-4 space-y-4 flex flex-col max-w-[150px]">
+            <Button>Primary</Button>
+            <Button>Primary</Button>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+        </div>
+    )
+};
+
+export default ButtonsPage;
