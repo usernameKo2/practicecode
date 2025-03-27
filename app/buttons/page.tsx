035808aff0e428a57b3d40c22b8a39bbfb493aed
usernameKo2 <jolom724@gmail.com>
2025-03-27 18:19:00 +0800

    try ulit

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 const ButtonsPage = () => {
     return(
         <div className="p-4 space-y-4 flex flex-col max-w-[150px]">
-            <Button>Primary</Button>
-            <Button>Primary</Button>
+            <Button variant="default">Primary</Button>
+            <Button variant="outline">Primary Outline</Button>
             <Button></Button>
             <Button></Button>
             <Button></Button>
